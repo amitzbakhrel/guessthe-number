5e4e82f4432c5e15623286ac708a807d38004155
amitzbakhrel <bakhrelamitz82@gmail.com>
2025-03-07 16:31:50 +0545

    mdd

---
+++ tictac.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+target = random.randint(1, 9)
+while True:
+    guess = int(input("enter your guess between 1 and 9"))
+    if guess == target:
+        print("congratulation you guessed right number")
+        break
+    elif guess > target:
+        print("your guess is too high")
+    else:
+        print("too low")
